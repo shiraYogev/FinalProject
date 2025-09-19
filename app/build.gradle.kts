0b234c6da71d2415724d38289fa6c3ddda06e3e0
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2025-09-19 14:27:46 +0300

    Update build.gradle.kts
    תצוגת כפתורים

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation ("com.google.android.flexbox:flexbox:3.0.0")
 
 }
 
