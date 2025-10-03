b8a3f7a8e5e3a049e304e7dd79abc8b2a4b80309
shirel1233 <155564582+shirel1233@users.noreply.github.com>
2025-10-03 10:31:49 +0300

    Update EditBankDetailsActivity.java

--- app/src/main/java/com/example/finalprojectappraisal/activity/newProject/bank/EditBankDetailsActivity.java
+++ app/src/main/java/com/example/finalprojectappraisal/activity/newProject/bank/EditBankDetailsActivity.java
@@ -15,7 +15,24 @@ import com.google.gson.Gson;
 
 public class EditBankDetailsActivity extends AppCompatActivity {
 
-    private TextInputEditText editBankName, editBranchName, editLoanerName, editLoanNumber;
+    // שדות בסיסיים
+    private TextInputEditText editBankName, editBranchName, editBranchEmail, editBankerName;
+
+    // שדות תאריכים
+    private TextInputEditText editDocumentDateGre, editDocumentDateHe, editAppraisalFinalDate;
+
+    // שדות הלוואה
+    private TextInputEditText editLoanNumber, editTypeOfLoan, editValuationNumber, editPurposeOfLoan;
+
+    // שדות לווה
+    private TextInputEditText editLoanerName, editLoanerId, editIdentityOfCustomer;
+
+    // שדות נכס
+    private TextInputEditText editLotNumber, editMainParcel, editSubParcel, editShortAddress;
+
+    // שדות נוספים
+    private TextInputEditText editPage1Header;
+
     private LinearProgressIndicator progressIndicator;
     private Button btnSave, btnCancel;
     private String pdfUriString, fileName, projectId;
@@ -23,16 +40,10 @@ public class EditBankDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_bank_details); // השם של קובץ ה-XML שלך
+        setContentView(R.layout.activity_edit_bank_details);
 
         // אתחול רכיבי ה-UI
-        editBankName = findViewById(R.id.editBankName);
-        editBranchName = findViewById(R.id.editBranchName);
-        editLoanerName = findViewById(R.id.editLoanerName);
-        editLoanNumber = findViewById(R.id.editLoanNumber);
-        btnSave = findViewById(R.id.btnSave);
-        btnCancel = findViewById(R.id.btnCancel);
-        progressIndicator = findViewById(R.id.progressIndicator);
+        initViews();
 
         // קבלת הנתונים מה-Intent
         Intent intent = getIntent();
@@ -45,7 +56,6 @@ public class EditBankDetailsActivity extends AppCompatActivity {
             if (bankDetailsJson != null) {
                 Gson gson = new Gson();
                 BankDetails bankDetails = gson.fromJson(bankDetailsJson, BankDetails.class);
-                // מילוי השדות בנתונים שחולצו
                 displayBankDetails(bankDetails);
             }
         }
@@ -60,30 +70,132 @@ public class EditBankDetailsActivity extends AppCompatActivity {
         });
     }
 
+    private void initViews() {
+        // שדות בסיסיים
+        editBankName = findViewById(R.id.editBankName);
+        editBranchName = findViewById(R.id.editBranchName);
+        editBranchEmail = findViewById(R.id.editBranchEmail);
+        editBankerName = findViewById(R.id.editBankerName);
+
+        // שדות תאריכים
+        editDocumentDateGre = findViewById(R.id.editDocumentDateGre);
+        editDocumentDateHe = findViewById(R.id.editDocumentDateHe);
+        editAppraisalFinalDate = findViewById(R.id.editAppraisalFinalDate);
+
+        // שדות הלוואה
+        editLoanNumber = findViewById(R.id.editLoanNumber);
+        editTypeOfLoan = findViewById(R.id.editTypeOfLoan);
+        editValuationNumber = findViewById(R.id.editValuationNumber);
+        editPurposeOfLoan = findViewById(R.id.editPurposeOfLoan);
+
+        // שדות לווה
+        editLoanerName = findViewById(R.id.editLoanerName);
+        editLoanerId = findViewById(R.id.editLoanerId);
+        editIdentityOfCustomer = findViewById(R.id.editIdentityOfCustomer);
+
+        // שדות נכס
+        editLotNumber = findViewById(R.id.editLotNumber);
+        editMainParcel = findViewById(R.id.editMainParcel);
+        editSubParcel = findViewById(R.id.editSubParcel);
+        editShortAddress = findViewById(R.id.editShortAddress);
+
+        // שדות נוספים
+        editPage1Header = findViewById(R.id.editPage1Header);
+
+        // כפתורים
+        btnSave = findViewById(R.id.btnSave);
+        btnCancel = findViewById(R.id.btnCancel);
+        progressIndicator = findViewById(R.id.progressIndicator);
+    }
+
     private void displayBankDetails(BankDetails bankDetails) {
         if (bankDetails != null) {
-            editBankName.setText(bankDetails.getBankName());
-            editBranchName.setText(bankDetails.getBranchName());
-            editLoanerName.setText(bankDetails.getLoanerName());
-            editLoanNumber.setText(bankDetails.getLoanNumber());
-            // TODO: מלא שדות נוספים מה-BankDetails (למשל, תאריכים וכו')
-            // editDocumentDateGre.setText(...);
-            // ...
+            // שדות בסיסיים
+            setTextSafely(editBankName, bankDetails.getBankName());
+            setTextSafely(editBranchName, bankDetails.getBranchName());
+            setTextSafely(editBranchEmail, bankDetails.getBranchEmail());
+            setTextSafely(editBankerName, bankDetails.getBankerName());
+
+            // שדות תאריכים
+            setTextSafely(editDocumentDateGre, bankDetails.getDocumentDateGre());
+            setTextSafely(editDocumentDateHe, bankDetails.getDocumentDateHe());
+            setTextSafely(editAppraisalFinalDate, bankDetails.getAppraisalFinalDate());
+
+            // שדות הלוואה
+            setTextSafely(editLoanNumber, bankDetails.getLoanNumber());
+            setTextSafely(editTypeOfLoan, bankDetails.getTypeOfLoan());
+            setTextSafely(editValuationNumber, bankDetails.getValuationNumber());
+            setTextSafely(editPurposeOfLoan, bankDetails.getPurposeOfLoan());
+
+            // שדות לווה
+            setTextSafely(editLoanerName, bankDetails.getLoanerName());
+            setTextSafely(editLoanerId, bankDetails.getLoanerId());
+            setTextSafely(editIdentityOfCustomer, bankDetails.getIdentityOfCustomer());
+
+            // שדות נכס
+            setTextSafely(editLotNumber, bankDetails.getLotNumber());
+            setTextSafely(editMainParcel, bankDetails.getMainParcel());
+            setTextSafely(editSubParcel, bankDetails.getSubParcel());
+            setTextSafely(editShortAddress, bankDetails.getShortAddress());
+
+            // שדות נוספים
+            setTextSafely(editPage1Header, bankDetails.getPage1Header());
         }
     }
 
-    private void saveBankDetails() {
-        //showLoading(true);
+    /**
+     * פונקציה עזר להגדרת טקסט בצורה בטוחה (מטפלת ב-null)
+     */
+    private void setTextSafely(TextInputEditText editText, String value) {
+        if (editText != null) {
+            editText.setText(value != null ? value : "");
+        }
+    }
 
-        // יצירת אובייקט BankDetails חדש מהנתונים המעודכנים מהשדות
+    /**
+     * פונקציה עזר לקריאת טקסט בצורה בטוחה
+     */
+    private String getTextSafely(TextInputEditText editText) {
+        if (editText != null && editText.getText() != null) {
+            return editText.getText().toString().trim();
+        }
+        return null;
+    }
+
+    private void saveBankDetails() {
+        // יצירת אובייקט BankDetails חדש מהנתונים המעודכנים
         BankDetails updatedBankDetails = new BankDetails();
-        updatedBankDetails.setBankName(editBankName.getText().toString());
-        updatedBankDetails.setBranchName(editBranchName.getText().toString());
-        updatedBankDetails.setLoanerName(editLoanerName.getText().toString());
-        updatedBankDetails.setLoanNumber(editLoanNumber.getText().toString());
-        // TODO: קח את הערכים משאר השדות ומלא אותם ב-updatedBankDetails
-        // updatedBankDetails.setDocumentDateGre(editDocumentDateGre.getText().toString());
-        // ...
+
+        // שדות בסיסיים
+        updatedBankDetails.setBankName(getTextSafely(editBankName));
+        updatedBankDetails.setBranchName(getTextSafely(editBranchName));
+        updatedBankDetails.setBranchEmail(getTextSafely(editBranchEmail));
+        updatedBankDetails.setBankerName(getTextSafely(editBankerName));
+
+        // שדות תאריכים
+        updatedBankDetails.setDocumentDateGre(getTextSafely(editDocumentDateGre));
+        updatedBankDetails.setDocumentDateHe(getTextSafely(editDocumentDateHe));
+        updatedBankDetails.setAppraisalFinalDate(getTextSafely(editAppraisalFinalDate));
+
+        // שדות הלוואה
+        updatedBankDetails.setLoanNumber(getTextSafely(editLoanNumber));
+        updatedBankDetails.setTypeOfLoan(getTextSafely(editTypeOfLoan));
+        updatedBankDetails.setValuationNumber(getTextSafely(editValuationNumber));
+        updatedBankDetails.setPurposeOfLoan(getTextSafely(editPurposeOfLoan));
+
+        // שדות לווה
+        updatedBankDetails.setLoanerName(getTextSafely(editLoanerName));
+        updatedBankDetails.setLoanerId(getTextSafely(editLoanerId));
+        updatedBankDetails.setIdentityOfCustomer(getTextSafely(editIdentityOfCustomer));
+
+        // שדות נכס
+        updatedBankDetails.setLotNumber(getTextSafely(editLotNumber));
+        updatedBankDetails.setMainParcel(getTextSafely(editMainParcel));
+        updatedBankDetails.setSubParcel(getTextSafely(editSubParcel));
+        updatedBankDetails.setShortAddress(getTextSafely(editShortAddress));
+
+        // שדות נוספים
+        updatedBankDetails.setPage1Header(getTextSafely(editPage1Header));
 
         // החזרת הנתונים למסך הקודם
         Intent resultIntent = new Intent();
@@ -106,4 +218,4 @@ public class EditBankDetailsActivity extends AppCompatActivity {
             btnCancel.setEnabled(true);
         }
     }
-}
+}.
